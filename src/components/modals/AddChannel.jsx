7ecfb110d86279d817b155da6dbdaec198156629
mn81566 <mikhail.taskaev@gmail.com>
2023-07-13 12:31:36 +0300

    Lint fixes 9

--- src/components/modals/AddChannel.jsx
+++ src/components/modals/AddChannel.jsx
@@ -33,7 +33,6 @@ const AddChannel = () => {
   const AddChannelSchema = yup.object().shape({
     // prettier-ignore
     channelName: yup.string()
-      .required()
       .min(3, 'От 3 до 20 символов!')
       .max(20, 'От 3 до 20 символов!')
       .notOneOf([channels.map((channel) => channel.name)], 'Должно быть уникальным'),
