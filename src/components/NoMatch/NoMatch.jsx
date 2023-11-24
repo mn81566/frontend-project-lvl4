0132a3240a819489fd925c0629ff2dd38c24d7c8
mn81566 <mikhail.taskaev@gmail.com>
2023-11-24 09:51:42 +0300

    404 page edit; delete unnessesary  files

--- src/components/NoMatch/NoMatch.jsx
+++ src/components/NoMatch/NoMatch.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import ROUTES from '../../routes.js';
 
-const NoMatch = () => {
-  const location = useLocation();
-
-  return (
-    <>
-      {/* <h1>404</h1>
-      <div>
-        No location
-        {' '}
-        <code>{location.pathname}</code>
-      </div> */}
-<div class="text-center"><img alt="Страница не найдена" class="img-fluid h-25" src="https://cdn2.hexlet.io/assets/error-pages/404-4b6ef16aba4c494d8101c104236304e640683fa9abdb3dd7a46cab7ad05d46e9.svg"><h1 class="h4 text-muted">Страница не найдена</h1><p class="text-muted">Но вы можете перейти <a href="/">на главную страницу</a></p></div>      
-    </>
-  );
-};
+const NoMatch = () => (
+  <div className="text-center">
+    <h1 className="h4 text-muted">404 Страница не найдена</h1>
+    <p className="text-muted">
+      Но вы можете перейти
+      <a href={ROUTES.root}> на главную страницу</a>
+    </p>
+  </div>
+);
 
 export default NoMatch;
